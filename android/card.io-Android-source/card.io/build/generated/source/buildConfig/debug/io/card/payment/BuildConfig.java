047d91c1e02c5550a10eeaadc4d04c6a4d4bba67
kmfree <kmfree@users.noreply.github.com>
2016-08-07 20:37:29 -0400

    WIP - Debugging. Needs documentation

---
+++ android/card.io-Android-source/card.io/build/generated/source/buildConfig/debug/io/card/payment/BuildConfig.java
@@ -0,0 +1,17 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package io.card.payment;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "io.card.payment";
+  public static final String BUILD_TYPE = "debug";
+  public static final String FLAVOR = "";
+  public static final int VERSION_CODE = 2;
+  public static final String VERSION_NAME = "1.0";
+  // Fields from default config.
+  public static final String BUILD_TIME = "08/07/2016 19:56:06 -0400";
+  public static final String PRODUCT_NAME = "card.io-Android-SDK";
+  public static final String PRODUCT_VERSION = "5.4.0-1-gb2d173a-dirty";
+}
