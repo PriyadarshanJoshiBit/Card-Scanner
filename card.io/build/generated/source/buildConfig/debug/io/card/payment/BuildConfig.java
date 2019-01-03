165a1e5eab2fae26eea8177c6d1982eb36d99930
Priyadarshan Joshi <priyadarshan.joshi@bitwiseglobal.com>
2019-01-03 17:35:36 +0530

    initial commit

---
+++ card.io/build/generated/source/buildConfig/debug/io/card/payment/BuildConfig.java
@@ -0,0 +1,17 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package io.card.payment;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "io.card.payment";
+  public static final String BUILD_TYPE = "debug";
+  public static final String FLAVOR = "";
+  public static final int VERSION_CODE = 2;
+  public static final String VERSION_NAME = "1.0";
+  // Fields from default config.
+  public static final String BUILD_TIME = "12/26/2018 12:03:11 +0530";
+  public static final String PRODUCT_NAME = "card.io-Android-SDK";
+  public static final String PRODUCT_VERSION = "845475f-dirty";
+}
